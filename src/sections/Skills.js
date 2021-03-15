d9c0bac79379bf3867be3e4b2d900c43cab9f2e2
Christopher Park <chris@gomarketbox.com>
2021-03-15 12:05:09 -0400

    python section and animations

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Chip from "@material-ui/core/Chip";
 import Avatar from "@material-ui/core/Avatar";
+import Fade from "react-reveal/Fade";
 import {
   MainHeader,
   SectionHeader,
@@ -23,48 +24,71 @@ import reactlogo from "../logos/reactlogo.png";
 import nodelogo from "../logos/nodelogo.png";
 import typescriptlogo from "../logos/typescriptlogo.png";
 import reduxlogo from "../logos/reduxlogo.png";
+import numpylogo from "../logos/numpylogo.png";
+import pandaslogo from "../logos/pandaslogo.png";
+import matplotliblogo from "../logos/matplotlib.png";
 
 const LanguagesSection = () => {
   return (
     <Grid container spacing={9}>
       <Grid item xs={12} sm={6}>
-        <InfoWindow elevation={10}>
-          <InfoTopic>
-            <LargeIcon src={javascriptlogo}></LargeIcon>
-            <WindowHeader>Javascript</WindowHeader>
-          </InfoTopic>
-          <InfoDetails>
-            <SmallIcon
-              avatar={<Avatar src={reactlogo} />}
-              label="React"
-              color="primary"
-            />
-            <SmallIcon
-              avatar={<Avatar src={nodelogo} />}
-              label="NodeJS"
-              color="primary"
-            />
-            <SmallIcon
-              avatar={<Avatar src={typescriptlogo} />}
-              label="Typescript"
-              color="primary"
-            />
-            <SmallIcon
-              avatar={<Avatar src={reduxlogo} />}
-              label="Redux"
-              color="primary"
-            />
-          </InfoDetails>
-        </InfoWindow>
+        <Fade left>
+          <InfoWindow elevation={10}>
+            <InfoTopic>
+              <LargeIcon src={javascriptlogo}></LargeIcon>
+              <WindowHeader>Javascript</WindowHeader>
+            </InfoTopic>
+            <InfoDetails>
+              <SmallIcon
+                avatar={<Avatar src={reactlogo} />}
+                label="React"
+                color="primary"
+              />
+              <SmallIcon
+                avatar={<Avatar src={nodelogo} />}
+                label="NodeJS"
+                color="primary"
+              />
+              <SmallIcon
+                avatar={<Avatar src={typescriptlogo} />}
+                label="Typescript"
+                color="primary"
+              />
+              <SmallIcon
+                avatar={<Avatar src={reduxlogo} />}
+                label="Redux"
+                color="primary"
+              />
+            </InfoDetails>
+          </InfoWindow>
+        </Fade>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <InfoWindow elevation={10}>
-          <InfoTopic>
-            <LargeIcon src={pythonlogo}></LargeIcon>
-            <WindowHeader>Python</WindowHeader>
-          </InfoTopic>
-          <InfoDetails></InfoDetails>
-        </InfoWindow>
+        <Fade right>
+          <InfoWindow elevation={10}>
+            <InfoTopic>
+              <LargeIcon src={pythonlogo}></LargeIcon>
+              <WindowHeader>Python</WindowHeader>
+            </InfoTopic>
+            <InfoDetails>
+              <SmallIcon
+                avatar={<Avatar src={numpylogo} />}
+                label="Numpy"
+                color="primary"
+              />
+              <SmallIcon
+                avatar={<Avatar src={pandaslogo} />}
+                label="Pandas"
+                color="primary"
+              />
+              <SmallIcon
+                avatar={<Avatar src={matplotliblogo} />}
+                label="Matplotlib"
+                color="primary"
+              />
+            </InfoDetails>
+          </InfoWindow>
+        </Fade>
       </Grid>
     </Grid>
   );
